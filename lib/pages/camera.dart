5f5b243a1736b245658064743df9ed42dfa17aed
CAVASOL <awyeon@gmail.com>
2024-02-01 11:58:35 +0900

    [Camera] mdfied UI

--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -10,169 +10,263 @@ class PickImage extends StatefulWidget {
   const PickImage({super.key});
 
   @override
-  State<PickImage> createState() => _PickImageState();
+  State<PickImage> createState() => _CameraPageState();
 }
 
-class _PickImageState extends State<PickImage> {
+class _CameraPageState extends State<PickImage> {
   Uint8List? _image;
   File? selectedImage;
+  List<String> modelSelectionOptions = [
+    "YOLO v5s",
+    "YOLO v5m",
+    "YOLO v5l",
+    "YOLO v5x"
+  ];
+  String selectedModel = "YOLO v5s";
+  TextEditingController imgSizeController = TextEditingController(text: "416");
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Stack(
-          children: [
-            _image != null
-                ? CircleAvatar(
-                    radius: 120,
-                    backgroundColor: Colors.white,
-                    backgroundImage: MemoryImage(
-                      _image!,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Upload your image, \nand select a YOLO v5 model",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                  )
-                : const CircleAvatar(
-                    radius: 120,
-                    backgroundColor: Colors.black,
                   ),
-            Positioned(
-              bottom: -0,
-              left: 160,
-              child: IconButton(
-                onPressed: () {
-                  showImagePickerOption(context);
-                },
-                icon: const Icon(
-                  Icons.add_a_photo,
-                ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Stack(
+                    children: [
+                      _image != null
+                          ? Container(
+                              width: 280,
+                              height: 200,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                color: Colors.grey.shade200,
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: MemoryImage(_image!),
+                                ),
+                              ),
+                            )
+                          : Container(
+                              width: 280,
+                              height: 200,
+                              color: Colors.grey.shade200,
+                            ),
+                      Positioned(
+                        bottom: -4,
+                        left: 232,
+                        child: IconButton(
+                          onPressed: () {
+                            showImagePickerOption(context);
+                          },
+                          icon: const Icon(
+                            Icons.add_a_photo,
+                          ),
+                          iconSize: 32,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: "Upload Images",
+                            ),
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: DropdownButtonFormField(
+                            value: selectedModel,
+                            items: modelSelectionOptions
+                                .map((selection) => DropdownMenuItem(
+                                      value: selection,
+                                      child: Text(selection),
+                                    ))
+                                .toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                selectedModel = value.toString();
+                              });
+                            },
+                            decoration: const InputDecoration(
+                              labelText: "Select YOLO Model",
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: TextFormField(
+                            controller: imgSizeController,
+                            decoration: const InputDecoration(
+                              labelText: "Model Inference Size",
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Handle form submission
+                            },
+                            child: const Text("Submit"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: SizedBox(
-        height: 90,
-        child: BottomAppBar(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 8,
-              left: 12,
-              right: 12,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/home',
-                    );
-                  },
-                  child: const Column(
-                    children: [
-                      Icon(
-                        Icons.home_outlined,
-                        color: Color(0xFF292929),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        '홈',
-                        style: TextStyle(
+          ),
+        ),
+        bottomNavigationBar: SizedBox(
+          height: 90,
+          child: BottomAppBar(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 8,
+                left: 12,
+                right: 12,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/home',
+                      );
+                    },
+                    child: const Column(
+                      children: [
+                        Icon(
+                          Icons.home_outlined,
                           color: Color(0xFF292929),
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                            color: Color(0xFF292929),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/search',
-                    );
-                  },
-                  child: const Column(
-                    children: [
-                      Icon(
-                        Icons.explore_outlined,
-                        color: Color(0xFF292929),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        '검색',
-                        style: TextStyle(
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/search',
+                      );
+                    },
+                    child: const Column(
+                      children: [
+                        Icon(
+                          Icons.explore_outlined,
                           color: Color(0xFF292929),
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                            color: Color(0xFF292929),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/camera',
-                    );
-                  },
-                  child: const Column(
-                    children: [
-                      Icon(
-                        Icons.camera_rounded,
-                        color: Color(0xFF292929),
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        '카메라',
-                        style: TextStyle(
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/camera',
+                      );
+                    },
+                    child: const Column(
+                      children: [
+                        Icon(
+                          Icons.camera_rounded,
                           color: Color(0xFF292929),
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'Camera',
+                          style: TextStyle(
+                            color: Color(0xFF292929),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/mypage',
-                    );
-                  },
-                  child: const Column(
-                    children: [
-                      Icon(
-                        Icons.person_outline_rounded,
-                        color: Color(0xFF292929),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        '나의 페이지',
-                        style: TextStyle(
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/mypage',
+                      );
+                    },
+                    child: const Column(
+                      children: [
+                        Icon(
+                          Icons.cases_outlined,
                           color: Color(0xFF292929),
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          'My Page',
+                          style: TextStyle(
+                            color: Color(0xFF292929),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
